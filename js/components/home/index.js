03f01ba02a31407fea30a7df42955383429468ee
powman <paulophpweb@gmail.com>
2017-02-08 09:42:40 -0200

    Update index.js

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -1,21 +1,16 @@
 
 import React, { Component } from 'react';
-import { TouchableOpacity,View, Text, AsyncStorage, ScrollView, Image, Alert } from 'react-native';
+import { View, AsyncStorage, Alert } from 'react-native';
+import { Container, Header, Content, Title, Icon, Button} from 'native-base';
 import { connect } from 'react-redux';
 import { actions } from 'react-native-navigation-redux-helpers';
-import { Grid, Row } from 'react-native-easy-grid';
+//import { Grid, Row } from 'react-native-easy-grid';
 
-import * as Animatable from 'react-native-animatable';
-import LinearGradient from 'react-native-linear-gradient';
-import { openDrawer } from '../../actions/drawer';
-import myTheme from '../../themes/base-theme';
+//import * as Animatable from 'react-native-animatable';
+//import LinearGradient from 'react-native-linear-gradient';
+//import myTheme from '../../themes/base-theme';
 import styles from './styles';
-
-
-const {
-  reset,
-  pushRoute,
-} = actions;
+const {reset,pushRoute} = actions;
 
 class Home extends Component {
 
@@ -43,48 +38,13 @@ class Home extends Component {
 
   render() {
     return (
-      <View style={styles.container}> 
-        <ScrollView
-        automaticallyAdjustContentInsets={false}
-        style={styles.scrollView}>
-          <View style={styles.meio}>
-            <Animatable.Image animation="bounceInDown" source={require('../../../images/logo_branca.png')} style={styles.logo}></Animatable.Image>
-            <View style={styles.bloco}>
-              <Text style={styles.texth1}>
-                Qual o seu tipo de acesso?
-              </Text>
-              <Text style={styles.texth2}>
-                Selecione se você irá emitir a sua localização ou se irá visualizar algum viajante.
-              </Text>
-              <TouchableOpacity style={styles.TouchableOpacity} onPress={() => this.logar()}>
-                <LinearGradient colors={['#ffffff','#ffffff']} style={styles.linearGradient}>
-                  <Text style={styles.buttonText}>
-                    Viajante
-                  </Text>
-                </LinearGradient>
-              </TouchableOpacity>
-              <Text style={styles.texth3}>
-                Ou
-              </Text>
-              <TouchableOpacity style={styles.TouchableOpacity} onPress={() => this.logar()}>
-                <LinearGradient colors={['#ffffff','#ffffff']} style={styles.linearGradient}>
-                  <Text style={styles.buttonText}>
-                    Observador
-                  </Text>
-                </LinearGradient>
-              </TouchableOpacity> 
-            </View>
-            <View style={styles.TextosRodape}>
-              <TouchableOpacity onPress={() => this.sair()}>
-                <Animatable.Text animation="flipInX" style={styles.TextoRodape1}>Sair</Animatable.Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => this.pushRoute('configuracoes',1)}>
-                <Animatable.Text animation="flipInX" style={styles.TextoRodape2}>Configurações</Animatable.Text>
-              </TouchableOpacity>
-            </View>
+      <Container>
+        <Content>
+          <View style={{flex: 1, height: screenHeight, justifyContent: 'center'}}>
+            <Button block> Login </Button>
           </View>
-        </ScrollView>
-      </View> 
+        </Content>
+      </Container>  
     );
   }
 }
@@ -97,8 +57,6 @@ function bindAction(dispatch) {
 }
 
 const mapStateToProps = state => ({
-  name: state.user.name,
-  list: state.list.list,
   navigation: state.cardNavigation,
 });
 
